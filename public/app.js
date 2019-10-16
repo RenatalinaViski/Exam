56c87242c6599007ed56c2431d15faf870fc5242
VISKI NATALINA <overlordnatalina@gmail.com>
2019-10-16 23:38:54 +0300

    test

---
+++ public/app.js
@@ -0,0 +1,21 @@
+let appendBody = (elem) => document.body.appendChild(document.createElement(elem))
+let appendToParent = (parent, elem) => parent.appendChild(document.createElement(elem))
+
+let divField = appendBody('div')
+divField.classList.add('w-500', 'br-black', 'h-500')
+
+let input = appendBody('input')
+input.style.width = '100'
+
+let button = appendBody('button')
+button.innerText = 'Push'
+button.onclick = () => {
+    let valueInput = input.value
+    fetch('/users', {
+        method: "POST",
+        headers: {
+            'Content-type': 'application/json',
+        },
+        body: JSON.stringify({name:valueInput})
+    })
+}
